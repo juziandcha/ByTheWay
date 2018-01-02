24179cdef74a2e2d787be7bfa0733f39f07f00cb
juziandcha <645140172@qq.com>
2018-01-02 10:22:47 +0800

    add_map_marker

--- app/src/main/java/com/example/wmj/bytheway/Dialog_historytask.java
+++ app/src/main/java/com/example/wmj/bytheway/Dialog_historytask.java
@@ -13,43 +13,59 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 /**
- * Created by wmj on 2017/12/31.
+ * Created by wmj on 2018/1/2.
  */
 
-public class Dialog_createtask extends DialogFragment{
+public class Dialog_historytask extends DialogFragment{
     private TextView title,content;
+    private TextView release_user,receive_user;
     private TextView start_address,end_address;
-    private Button task_create;
+    private TextView task_status;
 
-    private Dialog_createtask.DialogClickListener listener;
+    private Dialog_historytask.DialogClickListener listener;
 
     public interface DialogClickListener{
-        void onDialogClick(boolean ifcreate,String titlename,String contents,String s_address,String e_address);
+        void onDialogClick(boolean ifreceive);
     }
 
-    public void setOnDialogClick(Dialog_createtask.DialogClickListener listener){
+    public void setOnDialogClick(Dialog_historytask.DialogClickListener listener){
         this.listener=listener;
     }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
+        super.onCreate(savedInstanceState);
+        Bundle bundle=getArguments();
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = LayoutInflater.from(getActivity());
-        View view = inflater.inflate(R.layout.dialog_createtask, null);
+        View view = inflater.inflate(R.layout.dialog_histotytask, null);
+        title=(TextView)view.findViewById(R.id.title_history);
+        content=(TextView)view.findViewById(R.id.content_history);
+        release_user=(TextView)view.findViewById(R.id.release_user_history);
+        receive_user=(TextView)view.findViewById(R.id.receive_user_history);
+        start_address=(TextView)view.findViewById(R.id.saddress_history);
+        end_address=(TextView)view.findViewById(R.id.eaddress_history);
+        task_status=(TextView)view.findViewById(R.id.status_history);
 
-        title=(TextView)view.findViewById(R.id.title_create);
-        content=(TextView)view.findViewById(R.id.content_create);
-        start_address=(TextView)view.findViewById(R.id.saddress_create);
-        end_address=(TextView)view.findViewById(R.id.eaddress_create);
-        task_create=(Button)view.findViewById(R.id.create_task);
+        //获取预置信息
+        /*
+        title.setText(bundle.getString("title"));
+        content.setText(bundle.getString("content"));
+        release_user.setText(bundle.getString("release_user"));
+        receive_user.setText(bundle.getString("receive_user"));
+        start_address.setText(bundle.getString("start_address"));
+        end_address.setText(bundle.getString("end_address"));
+        task_status.setText(bundle.getString("task_status"));
+        */
         //设为其它状态不可选中
         //setCancelable(false);
 
         start_address.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //定位
+                //还需加入经纬度信息
                 Intent intent=new Intent(getActivity().getApplicationContext(),MapActivity.class);
                 startActivity(intent);
                 Toast.makeText(getActivity(),"star",Toast.LENGTH_SHORT).show();
@@ -58,17 +74,10 @@ public class Dialog_createtask extends DialogFragment{
         end_address.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //定位
+                //还需加入经纬度信息
                 Intent intent=new Intent(getActivity().getApplicationContext(),MapActivity.class);
                 startActivity(intent);
                 Toast.makeText(getActivity(),"end",Toast.LENGTH_SHORT).show();
-            }
-        });
-        task_create.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                listener.onDialogClick(true,title.getText().toString(),content.getText().toString(),start_address.getText().toString(),end_address.getText().toString());
-                dismiss();
             }
         });
 
@@ -76,4 +85,5 @@ public class Dialog_createtask extends DialogFragment{
 
         return builder.create();
     }
+
 }
--- app/src/main/java/com/example/wmj/bytheway/MapActivity.java
+++ app/src/main/java/com/example/wmj/bytheway/MapActivity.java
@@ -2,12 +2,110 @@ package com.example.wmj.bytheway;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.baidu.location.LocationClient;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.search.core.RouteLine;
+import com.baidu.mapapi.search.core.SearchResult;
+import com.baidu.mapapi.search.geocode.GeoCodeResult;
+import com.baidu.mapapi.search.geocode.GeoCoder;
+import com.baidu.mapapi.search.geocode.OnGetGeoCoderResultListener;
+import com.baidu.mapapi.search.geocode.ReverseGeoCodeOption;
+import com.baidu.mapapi.search.geocode.ReverseGeoCodeResult;
+import com.baidu.mapapi.search.route.RoutePlanSearch;
+import com.baidu.mapapi.utils.DistanceUtil;
 
 public class MapActivity extends AppCompatActivity {
+    private MapView mapView=null;
+    private BaiduMap baiduMap;
+    private GeoCoder mSearch = null; // 搜索模块，用于正向或反向编码
+    private LatLng latLng;
+    public LocationClient mLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_map);
+
+        //获取地图控件引用
+        mapView = (MapView) findViewById(R.id.bmapView);
+        baiduMap=mapView.getMap();
+
+        latLng=new LatLng(30.270258,120.126694);
+        MapStatusUpdate update= MapStatusUpdateFactory.newLatLng(latLng);
+        baiduMap.animateMapStatus(update);
+        update=MapStatusUpdateFactory.zoomTo(16f);
+        baiduMap.animateMapStatus(update);
+
+        //添加标记
+        BitmapDescriptor bitmapDescriptor= BitmapDescriptorFactory.fromResource(R.drawable.location);
+        OverlayOptions options=new MarkerOptions().position(latLng).icon(bitmapDescriptor);
+        baiduMap.addOverlay(options);
+
+        //反向获取地址编码
+        mSearch = GeoCoder.newInstance();
+        OnGetGeoCoderResultListener listener = new OnGetGeoCoderResultListener() {
+            public void onGetGeoCodeResult(GeoCodeResult result) {}
+
+            @Override
+            public void onGetReverseGeoCodeResult(ReverseGeoCodeResult result) {
+                if (result == null || result.error != SearchResult.ERRORNO.NO_ERROR) {
+                    //没有找到检索结果'
+                    return;
+                }
+                //获取反向地理编码结果
+                //addresstext.setText(result.getAddress());
+            }
+        };
+        mSearch.setOnGetGeoCodeResultListener(listener);
+
+        //双击地图选择位置
+        BaiduMap.OnMapDoubleClickListener Clicklistener = new BaiduMap.OnMapDoubleClickListener() {
+            public void onMapDoubleClick(LatLng point){
+                baiduMap.clear();
+                BitmapDescriptor bitmapDescriptor= BitmapDescriptorFactory.fromResource(R.drawable.location);
+                OverlayOptions options=new MarkerOptions().position(point).icon(bitmapDescriptor);
+                baiduMap.addOverlay(options);
+
+                //调用反向编码接口，触发listener，结果在listener函数中体现
+                ReverseGeoCodeOption reverseGeoCodeOption = new ReverseGeoCodeOption();
+                reverseGeoCodeOption.location(point);
+                mSearch.reverseGeoCode(reverseGeoCodeOption);
+            }
+        };
+        //百度双击监听事件
+        baiduMap.setOnMapDoubleClickListener(Clicklistener);
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mapView.onPause();
     }
 }
