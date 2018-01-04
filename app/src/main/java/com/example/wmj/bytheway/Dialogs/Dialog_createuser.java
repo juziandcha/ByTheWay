1404b1664d2731ac79eb57f40486609aa58c9b6a
juziandcha <645140172@qq.com>
2018-01-04 23:30:14 +0800

    add_new_dialog_register

--- app/src/main/java/com/example/wmj/bytheway/Dialogs/Dialog_createuser.java
+++ app/src/main/java/com/example/wmj/bytheway/Dialogs/Dialog_createuser.java
@@ -18,6 +18,7 @@ import android.widget.Toast;
 public class Dialog_createuser extends DialogFragment{
     private EditText Edit_name;
     private EditText Edit_password;
+    private EditText Edit_password2;
     private Button btn_create;
 
     private Dialog_createuser.DialogClickListener listener;
@@ -39,14 +40,26 @@ public class Dialog_createuser extends DialogFragment{
 
         Edit_name=(EditText)view.findViewById(R.id.user_create);
         Edit_password=(EditText)view.findViewById(R.id.password_create);
+        Edit_password2=(EditText)view.findViewById(R.id.password_create_again);
         btn_create=(Button)view.findViewById(R.id.create_new_user);
 
         btn_create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //数据库通信
-                Toast.makeText(getActivity(), Edit_name.getText().toString()+Edit_password.getText().toString(), Toast.LENGTH_SHORT).show();
-                dismiss();
+                String name=Edit_name.getText().toString();
+                String password1=Edit_password.getText().toString();
+                String password2=Edit_password2.getText().toString();
+
+                if(password1.equals(password2)){
+                    //数据库通信
+                    Toast.makeText(getActivity(), Edit_name.getText().toString()+Edit_password.getText().toString(), Toast.LENGTH_SHORT).show();
+                    dismiss();
+                }
+                else {
+                    Toast.makeText(getActivity(), "两次输入的密码不一致！", Toast.LENGTH_SHORT).show();
+                }
+
+
             }
         });
 
